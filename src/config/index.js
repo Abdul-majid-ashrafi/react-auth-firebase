7992938a062b5076897cc315f785f4ee03293b1b
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2020-10-20 13:19:02 +0500

    first commit

--- src/config/index.js
+++ src/config/index.js
@@ -12,14 +12,7 @@ export const httpRequst = axios.create({
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB_6XZ1hwfMa1dH6zL4E_HlDcNTaIxKWCg",
-    authDomain: "veziu-app.firebaseapp.com",
-    databaseURL: "https://veziu-app.firebaseio.com",
-    projectId: "veziu-app",
-    storageBucket: "veziu-app.appspot.com",
-    messagingSenderId: "737695062433",
-    appId: "1:737695062433:web:870d5b1c338530585fd3e0",
-    measurementId: "G-W1MJS7369K"
+   
 };
 
 firebase.initializeApp(firebaseConfig);
